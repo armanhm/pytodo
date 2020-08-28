cb2710008387f96ac874dd6e0b46711abb35d625
armanhm <41625842+armanhm@users.noreply.github.com>
2020-08-28 13:39:04 +0430

    Add files via upload

---
+++ todo.py
@@ -0,0 +1,76 @@
+
+import pickle
+import time
+
+def write_file(data):
+    file_tita = open('TITA.DO', 'wb')
+    pickle.dump(data, file_tita)
+    file_tita.close()
+    return "write is", data, "in TATA.DO"
+
+def load_file():
+   
+    file_tita = open('TITA.DO', 'rb')
+    data = pickle.load(file_tita)
+    file_tita.close()
+    return data
+
+print("You Todo with TITA-DO!")
+print("     TITA-DO v2")
+print("")
+print("")
+
+data_base = {}
+NUMBER_TODO = 1
+
+data = write_file(str(input("==> pleas enter your todo:")))
+data_base[load_file()] = NUMBER_TODO
+NUMBER_TODO += 1
+
+
+
+OPERATOR_CASE = str(input("If you want to continue?  1.Yes    2.No = "))
+while OPERATOR_CASE != "1" and OPERATOR_CASE != "2":
+    print("Your answer not found in program")
+    print("Enter correct answer please")
+    time.sleep(2)
+    OPERATOR_CASE = str(input("If you want to continue?  1.Yes    2.No = "))
+    if OPERATOR_CASE == "1" or OPERATOR_CASE == "2":
+        break
+
+
+
+while OPERATOR_CASE == "1":
+    try:
+        print("-> You Todo with TITA-DO! <-")
+    except:
+        print("Answer Error")
+        print("Your answer is not found in program")
+        print("Enter correct answer please")
+        time.sleep(0.5)
+        print("Your answer not found in program")
+        print("Enter correct answer please")
+
+    data = write_file(input(str("==>pleas enter your todo:")))
+    data_base[load_file()] = NUMBER_TODO
+    NUMBER_TODO += 1
+    OPERATOR_CASE = str(input("If you want to continue?  1.Yes    2.No = "))
+while OPERATOR_CASE != "1" and OPERATOR_CASE != "2":
+    print("Your answer not found in program")
+    print("Enter correct answer please")
+    time.sleep(0.5)
+    OPERATOR_CASE = str(input("If you want to continue?  1.Yes    2.No = "))
+    if OPERATOR_CASE == "1" or OPERATOR_CASE == "2":
+        break
+    if OPERATOR_CASE == "2":
+        break
+
+
+
+if OPERATOR_CASE == "2":
+    print("")
+    print(data_base)
+    print("  Thanks for using this program  ")
+    print("             Goodbye             ")
+    print("author", "==> TITA-DO <==", "Komeil Parseh")
+   
